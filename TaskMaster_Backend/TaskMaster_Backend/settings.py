4d8816470a6e04a59ea845574c868e3f9dd4be51
KolyazovKA <kolyazov.kirill-a@mail.ru>
2025-01-02 19:25:53 +0500

    Создал модель пользователя и сериализатор для него

--- TaskMaster_Backend/TaskMaster_Backend/settings.py
+++ TaskMaster_Backend/TaskMaster_Backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
 
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -106,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
--- TaskMaster_Backend/users/models.py
+++ TaskMaster_Backend/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    company_name = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(
+        max_length=50,
+        choices=[
+            ('admin', 'Admin'),
+            ('manager', 'Manager'),
+            ('user', 'User'),
+        ],
+        default='user'
+    )
+
+    def __str__(self):
+        return self.username
